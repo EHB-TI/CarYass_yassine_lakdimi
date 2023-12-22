198ea1aa19cade113177580db1cc999f4a72ab3a
yasysn <yassine.lakdimi@student.ehb.be>
2023-12-22 16:27:27 +0100

    make admin, seeder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function redirect()
     {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
+        if(Auth::id())
+        {
+            if(Auth::user()->typeUser=='0')
+            {
+                return view('user.home');
+            }
+            else
+            {
+                return view ('admin.home');
+            }
+        }
+        else
+        {
+            return redirect()->back();
+        }
     }
 }
