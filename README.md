342cca42893698ca0ec13d1a0b2255357652bde2
yasysn <yassine.lakdimi@student.ehb.be>
2024-01-15 16:59:34 +0100

    change background and add github

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/EHB-TI/CarYass_yassine_lakdimi
--- resources/views/site/welcome.blade.php
+++ resources/views/site/welcome.blade.php
@@ -4,7 +4,7 @@
 
 
 
-    <div class="hero-wrap ftco-degree-bg" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+    <div class="hero-wrap ftco-degree-bg" style="background-image: url('images/image_1.jpg');" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
         <div class="row no-gutters slider-text justify-content-start align-items-center justify-content-center">
